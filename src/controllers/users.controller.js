ae1769f274b4cf7e21b7f66a3fbcc8ca2d701e8d
IvoFiorentino <ivofiorentino0@gmail.com>
2023-10-17 21:33:13 -0300

    import solved

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,28 @@
+import { userService } from '../services/user.service.js';
+
+class UserController {
+  async createUser(req, res) {
+    try {
+      const newUser = await userService.createUser(req.body);
+      res.status(201).json({ user: newUser });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async findUserByUsername(req, res) {
+    const { username } = req.params;
+    try {
+      const user = await userService.findUserByUsername(username);
+      if (user) {
+        res.status(200).json({ user });
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
+
+export const userController = new UserController();
